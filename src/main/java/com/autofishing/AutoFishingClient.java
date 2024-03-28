e13c422035d218b35157c645c56cb9e01c7e4885
konoechoda <k4234230@163.com>
2024-03-29 00:27:51 +0800

    [update]

---
+++ src/main/java/com/autofishing/AutoFishingClient.java
@@ -0,0 +1,13 @@
+package com.autofishing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoFishingClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
