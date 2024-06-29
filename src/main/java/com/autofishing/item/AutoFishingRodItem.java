fa7673343505d90ab772287d90e0811ab59e8ec2
konoechoda <k4234230@163.com>
2024-06-29 16:23:01 +0800

    [update]

--- src/main/java/com/autofishing/item/AutoFishingRodItem.java
+++ src/main/java/com/autofishing/item/AutoFishingRodItem.java
@@ -3,17 +3,16 @@ package com.autofishing.item;
 import com.autofishing.register.ModItem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.item.*;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
-import net.minecraft.world.event.GameEvent;
+
+import java.lang.reflect.Field;
+import java.util.Objects;
+
 
 public class AutoFishingRodItem extends Item implements Vanishable {
 
@@ -25,21 +24,55 @@ public class AutoFishingRodItem extends Item implements Vanishable {
         @Override
         public void run() {
             while (true) {
-                ClientPlayerEntity player = MinecraftClient.getInstance().player;
-                if (player == null) {
+                ClientPlayerEntity user = MinecraftClient.getInstance().player;
+                if (user == null) {
                     return;
                 }
-                if(player.fishHook != null) {
-                    if (player.fishHook.getHookedEntity() != null) {
-                        ItemStack itemStack = player.getMainHandStack();
-                        boolean bl = itemStack.isOf(ModItem.AUTO_FISHING_ROD);
-                        if (bl) {
-                            // 模拟玩家右键点击行为
-                            TypedActionResult<ItemStack> result = itemStack.use(player.getWorld(), player, Hand.MAIN_HAND);
+                World world = MinecraftClient.getInstance().world;
+                if (world == null) {
+                    return;
+                }
+                World serverWorld = Objects.requireNonNull(MinecraftClient.getInstance().getServer()).getWorld(world.getRegistryKey());
+                if(serverWorld == null){
+                    return;
+                }
+                PlayerEntity playerEntity = serverWorld.getPlayerByUuid(user.getUuid());
+                if(playerEntity == null){
+                    return;
+                }
+                ItemStack itemStackRight = playerEntity.getMainHandStack();
+                ItemStack itemStackLeft = playerEntity.getOffHandStack();
+                if(itemStackRight.isOf(ModItem.AUTO_FISHING_ROD) || itemStackLeft.isOf(ModItem.AUTO_FISHING_ROD)) {
+                    if (playerEntity.fishHook != null) {
+                        FishingBobberEntity bobber = playerEntity.fishHook;
+                        try {
+                            // it's private field. so I need to use reflection to get it. but it's not recommended.
+                            Field hookCountdownFishField = FishingBobberEntity.class.getDeclaredField("hookCountdown");
+                            hookCountdownFishField.setAccessible(true);
+                            int hookCountdown = hookCountdownFishField.getInt(bobber);
+
+                            if (hookCountdown > 0) {
+                                if (itemStackRight.isOf(ModItem.AUTO_FISHING_ROD)) {
+                                    itemStackRight.use(world, user, Hand.MAIN_HAND);
+                                    itemStackRight.use(serverWorld, playerEntity, Hand.MAIN_HAND);
+                                } else if (itemStackLeft.isOf(ModItem.AUTO_FISHING_ROD)) {
+                                    itemStackLeft.use(world, user, Hand.OFF_HAND);
+                                    itemStackLeft.use(serverWorld, playerEntity, Hand.OFF_HAND);
+                                }
+                            }
+                        } catch (NoSuchFieldException | IllegalAccessException e) {
+                            throw new RuntimeException(e);
                         }
+
                     }
+                }else{
+                    return;
                 }
             }
+        }
+
+        public boolean isWaiting() {
+            return getState() == Thread.State.WAITING || getState() == Thread.State.TIMED_WAITING;
         }
     }
 
@@ -48,43 +81,47 @@ public class AutoFishingRodItem extends Item implements Vanishable {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
-        if (user.fishHook != null) {
-            if (!world.isClient) {
-                int i = user.fishHook.use(itemStack);
-                itemStack.damage(i, user, p -> p.sendToolBreakStatus(hand));
-            }
-            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.NEUTRAL, 1.0f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
-            user.emitGameEvent(GameEvent.ITEM_INTERACT_FINISH);
-        } else {
-            world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
-            if (!world.isClient) {
-                int i = EnchantmentHelper.getLure(itemStack);
-                int j = EnchantmentHelper.getLuckOfTheSea(itemStack);
-                FishingBobberEntity fishingBobberEntity = new FishingBobberEntity(user, world, j, i);
-                world.spawnEntity(fishingBobberEntity);
-            }
-            user.incrementStat(Stats.USED.getOrCreateStat(this));
-            user.emitGameEvent(GameEvent.ITEM_INTERACT_START);
-        }
 
         ItemStack itemStackRight = user.getMainHandStack();
-        boolean bl = itemStackRight.isOf(ModItem.AUTO_FISHING_ROD);
-        if (bl) {
-            if (!world.isClient) {
-                if(!autoThread.isAlive()){
-                    autoThread = new AutoThread();
-                    autoThread.start();
+        ItemStack itemStackLeft = user.getOffHandStack();
+
+        if (user.isAlive()) {
+
+            if(user.fishHook != null) {
+                if (itemStackRight.isOf(Items.FISHING_ROD)) {
+                    itemStack.use(world, user, Hand.MAIN_HAND);
+                } else if (itemStackLeft.isOf(Items.FISHING_ROD)) {
+                    itemStack.use(world, user, Hand.OFF_HAND);
                 }
             }
-        }else {
-            try {
-                if(autoThread.isAlive())
-                    autoThread.wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+
+            if (itemStackRight.isOf(Items.FISHING_ROD)) {
+                itemStack.use(world, user, Hand.MAIN_HAND);
+            } else if (itemStackLeft.isOf(Items.FISHING_ROD)) {
+                itemStack.use(world, user, Hand.OFF_HAND);
+            }
+
+            if (!world.isClient()) {
+                return TypedActionResult.success(itemStack, world.isClient());
+            }
+            if (itemStackRight.isOf(ModItem.AUTO_FISHING_ROD) || itemStackLeft.isOf(ModItem.AUTO_FISHING_ROD)) {
+                if (!autoThread.isAlive()) {
+                    autoThread = new AutoThread();
+                    autoThread.start();
+                } else if (autoThread.isWaiting()) {
+                    autoThread.notify();
+                }
+            } else {
+                try {
+                    if (autoThread.isAlive())
+                        autoThread.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
         return TypedActionResult.success(itemStack, world.isClient());
+
     }
 
     @Override
